cb091fe7f9ca7c4fe4372f35f5c5c9d256587c97
Muhammad <talhayounus065@gmail.com>
2025-08-27 15:46:00 +0500

    added the yap in input fields of adding user

--- ecomerence/src/components/addClient/AddClient.js
+++ ecomerence/src/components/addClient/AddClient.js
@@ -1,24 +1,45 @@
-
+import * as yup from 'yup';
 import './addclient.css'
 import { useState } from 'react';
 export default function AddClient(props){
   const [addname, setname] = useState("");
   const [addemail, setemail] = useState("");
   const [addpassword, setpassword ] = useState("");
+  const [error, seterror] = useState("");
   
+  
+  let schema = yup.object().shape({
+    username: yup.string().required("Username is required").min(3, "Username must be at least 3 characters"),
+    email: yup.string().email("Invalid email format").required("Email is required"),  
+    password: yup.string().required("Password is required").min(6, "Password must be at least 6 characters"),
+  });
+const ClickHandler = async () =>{  
    let data = {
     username: addname,
     email: addemail,
     password: addpassword,
    }
+  try {
+   let result = await schema.validateSync(data);
+  props.AddNewUser(data) 
+setname("");
+setemail(""); 
+setpassword("");
+seterror("" );
+  }
   
+  catch (error) {
+  
+    seterror(error.toString());}
+  }
 
     return(
          <div>
+          <div style={{color:"red"}}>{error}</div>
                   <input onChange={(e)=>setname(e.target.value)} type="text" id="addusername" placeholder='username'/>
                   <input onChange={(e)=>setemail(e.target.value)} type="text" id="addemail" placeholder='email'/>
                   <input onChange={(e)=>setpassword(e.target.value)} type="text" id="addpassword" placeholder='password'/>
-                <button onClick={()=>props.AddNewUser(data)} style={{backgroundColor: "green", fontWeight: "bold"} }>Add New User</button>
+                <button onClick={()=>ClickHandler()} style={{backgroundColor: "green", fontWeight: "bold"} }>Add New User</button>
                 </div>
     )
 }
